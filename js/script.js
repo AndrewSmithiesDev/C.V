31884cac0d6197e67ca588c0fe497f35644c63b5
Andrew Smithies <andrewsmithies.dev@gmail.com>
2025-07-23 10:09:26 +0200

    Added bootstrap tooltips and used on language icons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+// Nav menu hamburger
 const hamburger = document.querySelector("#toggle-btn");
 
 hamburger.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+
+//Bootstrap tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
